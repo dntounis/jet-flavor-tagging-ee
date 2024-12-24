1dd3a4481e20ffe716878244a9389b31bc574069
Dimitrios Ntounis <dimitris.ntounis@gmail.com>
2024-12-24 06:37:47 -0800

    restructure folders for whizard

--- whizard/s3df_slurm_submission_setup/myJobPayload.sh
+++ whizard/s3df_slurm_submission_setup/myJobPayload.sh
@@ -9,26 +9,23 @@ echo "Running Whizard for flavor $flavor with number ${number}"
 
 #cd Hss_setup_test/whizard_v2/
 #cd /fs/ddn/sdf/group/atlas/d/dntounis/Hss_setup_test/whizard_v2/$flavor
-cd __WHIZARD_DIR__/$flavor
+cd __WHIZARD_DIR__/$flavor/$process
 
 #source /cvmfs/sw.hsf.org/spackages6/key4hep-stack/2022-12-23/x86_64-centos7-gcc11.2.0-opt/ll3gi/setup.sh
 source /cvmfs/sw-nightlies.hsf.org/key4hep/setup.sh
 
 
-SIN_FILE="wzp6_ee_nunuH_H${flavor}_ecm250.sin"
-TEMP_SIN_FILE="wzp6_ee_nunuH_H${flavor}_ecm250_${number}.sin"
-
 # choose one from Z,nunuH and nunuHH
 
 
 if [[ "$process" == "Z" ]]; then
-    SIN_FILE="wzp6_ee_Z${flavor}_ecm91p2.sin"
+    SIN_FILE="../wzp6_ee_Z${flavor}_ecm91p2.sin"
     TEMP_SIN_FILE="wzp6_ee_Z${flavor}_ecm91p2_${number}.sin"
 elif [[ "$process" == "nunuH" ]]; then
-    SIN_FILE="wzp6_ee_nunuH_H${flavor}_ecm250.sin"
+    SIN_FILE="../wzp6_ee_nunuH_H${flavor}_ecm250.sin"
     TEMP_SIN_FILE="wzp6_ee_nunuH_H${flavor}_ecm250_${number}.sin"
 elif [[ "$process" == "nunuHH" ]]; then
-    SIN_FILE="wzp6_ee_nunuHH_H${flavor}_H${flavor}_ecm550.sin"
+    SIN_FILE="../wzp6_ee_nunuHH_H${flavor}_H${flavor}_ecm550.sin"
     TEMP_SIN_FILE="wzp6_ee_nunuHH_H${flavor}_H${flavor}_ecm550_${number}.sin"
 fi
 
